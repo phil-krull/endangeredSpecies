a192ee6fdd3731d98a257fa913ac3808b61141c0
phil-krull <pkrull@codingdojo.com>
2018-08-10 12:45:40 -0500

    test

--- server/config/routes.js
+++
@@ -1,7 +0,0 @@
-const animals = require("./../controllers/animals");
-
-module.exports = (app)=>{
-    app.get("/animals", animals.index),
-    app.post("/animals", animals.create),
-    app.get("/animals/:id", animals.show)
-}
